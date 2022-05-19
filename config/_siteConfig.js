abb69bf89241cd47a3c8433d8fe05e092c64978e
marvinchamata <89401259+marvinchamata@users.noreply.github.com>
2022-05-19 14:31:09 +0200

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -9,8 +9,8 @@ import theCMS from '../cms/netlify/cms'
  * General Site configurations
  */
 export default {
-  siteName: 'Awake',
-  tagline: 'A Nuxt.js Theme for Netlify CMS',
+  siteName: 'SanjivaniAfrica Pharmaceuticals',
+  tagline: 'Med for Life',
   featureImage: '/uploads/home-hero.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
@@ -27,16 +27,16 @@ export default {
       link: '/contact'
     },
     {
-      name: 'Categories',
+      name: 'Products',
       link: '/categories'
     },
     {
-      name: 'Github Repo',
+      name: 'What We Do',
       link: 'https://github.com/danielkellyio/awake-template',
       target: '_blank'
     },
     {
-      name: 'Awake+ Premium',
+      name: 'Team',
       link: 'https://awake-premium-lp.netlify.com/',
       target: '_blank'
     }
