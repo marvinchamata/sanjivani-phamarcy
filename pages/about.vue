2a0cb027480f17bad1c4a7b664655cb18ab7a2a4
marvinchamata <89401259+marvinchamata@users.noreply.github.com>
2022-05-21 13:35:23 +0200

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,32 +2,41 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
       title="About Us"
-      subtitle="Example About Page"
+      subtitle="About Sanjivani Pharmaceuticals"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
+          <h3> About Us</h3>
           <p>
             <strong>
               Edit this page in<code>/pages/about.vue</code>to fit your needs.
             </strong>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            With offices in SADC regions, Sanjivani Pharmaceutical works with many of the world’s leading
+companies through various agreements, partnerships and joint ventures, the group is a leading
+healthcare, medical equipment and consumer care company in Zambia , Botswana, Mauritius
+India and is committed to meeting the growing needs of healthcare and pharmaceutical
+distribution in the regions
+Sanjivani Pharmaceutical has a unique distribution and marketing network of more than 50 sales
+and medical representatives(comprising mostly of doctors), as well as wholesale organization and
+chain of local pharmacies, the company is managed by a highly experienced team consiting of
+SADC regions and expatriates with diverse backgrounds and skill sets
           </p>
 
           <h3>Another Fake Heading</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
+          <p>Sanjivani Pharmaceuticals is a pharmaceutical wholesaler and distributor committed to delivering
+a high standard of service to national and international partners in the healthcare sector.
+
+     </p> 
+     
+     <p> Our long term experience in trading pharmaceutical
+products worldwide makes us a reliable partner in
+sourcing pharmaceutical products with the highest level
+of quality and safety.
+     </p>
           <quote-card />
           <h3>Still More Fake Content</h3>
           <p>
