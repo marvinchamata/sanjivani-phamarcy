175c07e57d13b28289c316f1567c79d2f0a82a5d
marvinchamata <89401259+marvinchamata@users.noreply.github.com>
2022-05-23 14:43:13 +0200

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -45,15 +45,15 @@ export default {
 }
 .logo-wrapper {
   position: relative;
-  height: 80px;
+  height: 100px;
   overflow: hidden;
   width: 60px;
   .logo-inner {
     position: absolute;
-    height: 80px;
+    height: 100px;
     img {
       display: block;
-      height: 80px;
+      height: 100px;
       margin: 5px;
       max-height: initial;
     }
