449eaa166e9ab172c9a2ab169c7aee38560df487
marvinchamata <89401259+marvinchamata@users.noreply.github.com>
2022-05-23 17:27:13 +0200

    Update index.vue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
-  
-  <h3>ACTIVE PHARMACEUTICAL INGREDIENTS<h3/>
-  
     <site-hero title="Products" image="https://picsum.photos/1800/1801" />
     <main-section theme="one-column">  
       <categories-grid />
