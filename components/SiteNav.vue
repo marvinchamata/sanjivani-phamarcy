afa2a1da7d17789f25d2caa23f71fa5f88ba1cb1
marvinchamata <89401259+marvinchamata@users.noreply.github.com>
2022-05-23 14:11:55 +0200

    Update SiteNav.vue

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -28,7 +28,7 @@
           v-for="item in $siteConfig.mainMenu"
           :key="item.link"
           class="navbar-item"
-          @click="active = false"
+          @click="active = true"
         >
           <component
             :is="item.link.startsWith('http') ? 'a' : 'nuxt-link'"
